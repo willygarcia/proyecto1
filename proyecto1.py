b601445a9c1d214139580a1d38777c173381db97
willygarcia <william.alex2010@hotmail.com>
2016-05-25 19:26:29 -0600

    segundo proyecto

--- proyecto1.py
+++ proyecto1.py
@@ -1,1 +1,7 @@
-print ("hola mundo")
+def num (a):
+  	a = int(input("ingrese un numero"))
+	if a % 2 == 2:
+		print("Es un numero par")
+	else:
+		print("Es un numero impar")
+	return num
---
+++ proyecto2.py
@@ -0,0 +1,3 @@
+def prob2 (F):
+	celsius = (F - 32)* 5/9
+	print(celsius)
